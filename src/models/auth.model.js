4139f9139042ced5a2499ffcbbf2f91fb166b2f0
Akash Singh <manduakash@gmail.com>
2026-06-26 08:23:01 +0530

    akash

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -16,7 +16,7 @@ export const AuthModel = {
     const rows = await db
       .select()
       .from(userMaster)
-      .where(and(eq(userMaster.userName, userName), eq(userMaster.password, password)))
+      .where(and(eq(userMaster.userName, userName), eq(userMaster.password, btoa(password))))
       .limit(1);
 
     const user = rows[0];
